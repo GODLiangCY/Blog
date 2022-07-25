462c936a275c13e8d17ff1d953d4f8c37e6e3eb4
Young <younggglcy@gmail.com>
2022-07-25 22:43:49 +0800

    feat: footer

---
+++ src/components/FooterBar.vue
@@ -0,0 +1,18 @@
+<script setup lang='ts'>
+const route = useRoute()
+
+</script>
+
+<template>
+  <div class="mt-10 mb-6 prose m-auto opacity-50 flex">
+    <span class="text-sm"><a target="_blank" href="https://creativecommons.org/licenses/by-nc-sa/4.0/" style="color:inherit">CC BY-NC-SA 4.0</a> 2022-PRESENT © GODLiangCY</span>
+    <span text-sm v-if="route.path === '/'"> &nbsp;&nbsp;|&nbsp;&nbsp; <a target="_blank" href="https://beian.miit.gov.cn/" style="color:inherit">浙ICP备2021035264号-2</a> </span>
+    <div class="flex-auto" />
+  </div>
+</template>
+
+
+<style scoped>
+
+
+</style>
