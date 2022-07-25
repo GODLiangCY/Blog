b7ff2116151594dbab297eb65e6e8f63aa74c97b
Young <younggglcy@gmail.com>
2022-07-25 19:43:25 +0800

    feat: allow filter posts by tags

--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -2,6 +2,10 @@
 import type { Post } from '~/types'
 import it from 'markdown-it'
 
+defineEmits<{
+  (e: 'tagClick', name: string): void
+}>()
+
 const props: Readonly<Post> = defineProps<{
   categories: string[],
   date: string,
@@ -29,9 +33,15 @@ const desc = md.render(props.description)
     <div flex items-center justify-between>
       <span flex items-center><i-ant-design:calendar-twotone mr-3 />{{ props.date }}</span>
       <span flex items-center><i-bi:book mr-3 />{{ props.words }}</span>
-      <span flex items-center><i-fluent:timer-24-filled mr-3 />{{ props.duration }}</span>
+      <span flex items-center><i-ep:timer mr-3 />{{ props.duration }}</span>
     </div>
     <div v-html="desc" class="prose mt-1">
+    </div>
+    <div flex items-center flex-wrap>
+      <div v-for="tag of props.tags" :key="tag" flex items-center mt-4 class="basis-1/4">
+        <i-carbon:tag-group hover:cursor-pointer @click="$emit('tagClick', tag)" />
+        <span hover:cursor-pointer hover:underline @click="$emit('tagClick', tag)" >{{ tag }}</span>
+      </div>
     </div>
   </div>
 </template>
