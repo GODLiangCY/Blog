6a301525c0eb0ec54b6c72e73191f6ff14d72152
Young <younggglcy@gmail.com>
2022-07-25 18:59:35 +0800

    feat: rss

---
+++ scripts/rss.ts
@@ -0,0 +1,86 @@
+import { dirname } from 'path'
+import fg from 'fast-glob'
+import fs from 'fs-extra'
+import matter from 'gray-matter'
+import MarkdownIt from 'markdown-it'
+import type { FeedOptions, Item } from 'feed'
+import { Feed } from 'feed'
+import dayjs from 'dayjs'
+import customParseFormat from 'dayjs/plugin/customParseFormat'
+
+dayjs.extend(customParseFormat)
+
+const DOMAIN = 'https://godliangcy.ink'
+const AUTHOR = {
+  name: 'GODLiangCY',
+  email: 'younggglcy@gmail.com',
+  link: DOMAIN,
+}
+const markdown = MarkdownIt({
+  html: true,
+  breaks: true,
+  linkify: true,
+})
+
+async function run() {
+  await buildBlogRSS()
+}
+
+async function buildBlogRSS() {
+  const files = await fg('pages/posts/*.md')
+
+  const options = {
+    title: 'GODLiangCY',
+    description: 'GODLiangCY\' Blog',
+    id: 'https://godliangcy.ink/',
+    link: 'https://godliangcy.ink/',
+    copyright: 'CC BY-NC-SA 4.0 2022 © GODLiangCY',
+    feedLinks: {
+      json: 'https://godliangcy.ink/feed.json',
+      atom: 'https://godliangcy.ink/feed.atom',
+      rss: 'https://godliangcy.ink/feed.xml',
+    },
+  }
+  const posts: any[] = (
+    await Promise.all(
+      files.filter(i => !i.includes('index'))
+        .map(async(i) => {
+          const raw = await fs.readFile(i, 'utf-8')
+          const { data, content } = matter(raw)
+
+          const html = markdown.render(content)
+            .replace('src="/', `src="${DOMAIN}/`)
+
+          return {
+            ...data,
+            date: new Date(dayjs(data.date, 'YYYY-MM-DD').valueOf()),
+            content: html,
+            author: [AUTHOR],
+            link: DOMAIN + i.replace(/^pages(.+)\.md$/, '$1'),
+          }
+        }),
+    ))
+    .filter(Boolean)
+
+  posts.sort((a, b) => dayjs(b.date, 'YYYY-MM-DD').valueOf() - dayjs(a.date, 'YYYY-MM-DD').valueOf())
+
+  await writeFeed('feed', options, posts)
+}
+
+async function writeFeed(name: string, options: FeedOptions, items: Item[]) {
+  options.author = AUTHOR
+  options.image = 'https://godliangcy.ink/avatar.jpg'
+  options.favicon = 'https://godliangcy.ink/vite.svg'
+
+  const feed = new Feed(options)
+
+  items.forEach(item => feed.addItem(item))
+  // items.forEach(i=> console.log(i.title, i.date))
+
+  await fs.ensureDir(dirname(`./dist/${name}`))
+  await fs.writeFile(`./dist/${name}.xml`, feed.rss2(), 'utf-8')
+  await fs.writeFile(`./dist/${name}.atom`, feed.atom1(), 'utf-8')
+  await fs.writeFile(`./dist/${name}.json`, feed.json1(), 'utf-8')
+}
+
+run()
