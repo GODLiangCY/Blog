ee67c72521b993706d73412cbdf903ff96dc923f
Young <younggglcy@gmail.com>
2022-07-25 19:43:02 +0800

    feat: scroll behavior

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,24 @@ import '~/styles/main.css'
 import '~/styles/markdown.css'
 import '~/styles/prose.css'
 import App from './App.vue'
-
 import routes from '~pages'
+import type { RouterScrollBehavior } from 'vue-router'
 
-export const app = ViteSSG(
+const scrollBehavior: RouterScrollBehavior = (to, from, savedPosition) => {
+  if (to.hash) {
+    return {
+      el: decodeURIComponent(to.hash),
+      top: 20,
+      behavior: 'smooth',
+    }
+  } else if (savedPosition) {
+    return savedPosition
+  } else {
+    return { top: 0, behavior: 'smooth' }
+  }
+}
+
+export const createApp = ViteSSG(
   App,
-  { routes },
+  { routes, scrollBehavior },
 )
