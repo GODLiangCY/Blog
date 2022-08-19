58c259642c15f130788ec41733c6876147b95060
Young Liang <younggglcy@gmail.com>
2022-08-19 18:11:53 +0800

    feat: infinite-scroll

---
+++ src/utils/dom.ts
@@ -0,0 +1,47 @@
+export const isScroll = (el: HTMLElement, isVertical?: boolean): boolean => {
+  const key = (
+    {
+      undefined: 'overflow',
+      true: 'overflow-y',
+      false: 'overflow-x',
+    } as const
+  )[String(isVertical)]!
+  const overflow = (el.style as any)[key]
+  return ['scroll', 'auto', 'overlay'].some((s) => overflow.includes(s))
+}
+
+export const getScrollContainer = (
+  el: HTMLElement,
+  isVertical?: boolean
+): Window | HTMLElement | undefined => {
+  let parent: HTMLElement = el
+  while (parent) {
+    if ([window, document, document.documentElement].includes(parent))
+      return window
+
+    if (isScroll(parent, isVertical)) return parent
+
+    parent = parent.parentNode as HTMLElement
+  }
+
+  return parent
+}
+
+export const getOffsetTop = (el: HTMLElement) => {
+  let offset = 0
+  let parent = el
+
+  while (parent) {
+    offset += parent.offsetTop
+    parent = parent.offsetParent as HTMLElement
+  }
+
+  return offset
+}
+
+export const getOffsetTopDistance = (
+  el: HTMLElement,
+  containerEl: HTMLElement
+) => {
+  return Math.abs(getOffsetTop(el) - getOffsetTop(containerEl))
+}
