554f27adb2468b5afa6571b1818593d07bd5a712
Young <younggglcy@gmail.com>
2022-11-16 01:16:51 +0800

    feat: enable markdown code block hightlight

---
+++ plugins/workers/highlightWorker.ts
@@ -0,0 +1,33 @@
+import type { Highlighter, HtmlRendererOptions, ILanguageRegistration, IThemeRegistration } from 'shiki'
+import { getHighlighter as _get } from 'shiki'
+import { runAsWorker } from 'synckit'
+
+let highlighter: Highlighter
+
+function getHighlighter(command: 'getHighlighter', options?: {
+  themes: IThemeRegistration[]
+  langs: ILanguageRegistration[]
+}): void
+
+function getHighlighter(command: 'codeToHtml', options: {
+  code: string
+  lang: string
+  lineOptions?: HtmlRendererOptions['lineOptions']
+  theme?: IThemeRegistration
+}): Promise<string>
+
+async function getHighlighter(command: 'getHighlighter' | 'codeToHtml', options: any) {
+  if (!highlighter && command === 'getHighlighter') { highlighter = await _get(options ?? {}) }
+  else if (command === 'codeToHtml') {
+    const { code, lang, lineOptions, theme } = options
+    return highlighter.codeToHtml(code, {
+      lang,
+      lineOptions: lineOptions ?? [],
+      theme,
+    })
+  }
+}
+
+export type TypeSyncRun = typeof getHighlighter
+
+runAsWorker(getHighlighter)
